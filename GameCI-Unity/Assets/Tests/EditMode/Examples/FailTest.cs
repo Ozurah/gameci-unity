8e580525d4874f6aecea07f85109489cf74433b9
Ozurah <80265286+Ozurah@users.noreply.github.com>
2025-08-20 15:46:08 +0200

    test: Ajout d'un test qui échoue
    Pour voir le comportement de la CI

---
+++ GameCI-Unity/Assets/Tests/EditMode/Examples/FailTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using Project.Example;
+
+namespace Project.Tests.EditMode.Example
+{
+    [TestFixture]
+    public class FailTest
+    {
+        [Test]
+        public void EditModeExampleTestSimplePasses()
+        {
+            Assert.Fail();
+        }
+    }
+}
